b70311815d6292f08340c1a27a7a68bc77c8cc1d
lixuling <antonisaputra049@gmail.com>
2022-08-29 09:06:51 +0700

    menambah todo list

--- public/app.js
+++ public/app.js
@@ -130,14 +130,16 @@ const formDiv = document.getElementById("form");
 
 function Form() {
   const [nama, setNama] = React.useState("");
+  const h1 = React.useRef(null);
 
   function submit(event) {
     event.preventDefault();
+    h1.current.textContent = `terima kasih ${nama}`;
     console.log("nama: " + nama);
   }
 
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    className: "flex justify-center bg-slate-200 w-72 rounded-lg mx-auto h-auto py-5 mb-32"
+    className: "flex justify-center bg-slate-200 w-72 rounded-lg mx-auto h-auto py-5 mb-10"
   }, /*#__PURE__*/React.createElement("form", {
     action: "",
     className: "px-4"
@@ -146,7 +148,7 @@ function Form() {
   }, "nama"), /*#__PURE__*/React.createElement("input", {
     type: "text",
     name: "nama",
-    id: "",
+    value: nama,
     className: "outline-none border border-sky-300 ring-1 ring-sky-300 rounded-lg px-4 py-1 focus:border-sky-300 focus:border focus:ring-1 focus:ring-sky-300",
     onChange: function (event) {
       setNama(event.target.value);
@@ -158,7 +160,123 @@ function Form() {
     value: "submit",
     className: "py-2 px-3 shadow-slate-400 shadow-lg rounded-lg cursor-pointer",
     onClick: submit
-  })))));
+  })))), /*#__PURE__*/React.createElement("h1", {
+    ref: h1,
+    className: "text-center text-slate-800 text-xl font-semibold mb-10"
+  }));
 }
 
-ReactDOM.render( /*#__PURE__*/React.createElement(Form, null), formDiv);
+ReactDOM.render( /*#__PURE__*/React.createElement(Form, null), formDiv); //fetch API
+
+const apiDiv = document.getElementById("fetch");
+
+function Fetch() {
+  let [title, setTitle] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    const getData = async () => {
+      const getNews = await fetch("https://newsapi.org/v2/everything?q=keyword&apiKey=f2885538eab2402788c28125bc6de076");
+      const news = await getNews.json();
+      const articles = news.articles;
+      setTitle(articles);
+      setLoading(false);
+    };
+
+    getData();
+  }, []);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    className: "text-center text-3xl font-bold text-slate-800 mb-5 uppercase"
+  }, "judul berita"), /*#__PURE__*/React.createElement("div", {
+    className: "overflow-scroll h-56 w-1/2 mx-auto"
+  }, /*#__PURE__*/React.createElement("ul", {
+    className: "list-decimal"
+  }, loading && /*#__PURE__*/React.createElement("h1", {
+    className: "text-center text-1xl font-bold text-slate-800 mb-5 uppercase"
+  }, "masih loading bang"), !loading && title.map(value => {
+    return /*#__PURE__*/React.createElement("li", null, value.title);
+  }))));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Fetch, null), apiDiv); //to do list 
+
+const todoDiv = document.getElementById("todolist");
+
+function Todo() {
+  const [activity, setActivity] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+  const [edit, setEdit] = React.useState({});
+  React.useEffect(function () {
+    console.log(todos);
+  }, [todos]);
+
+  function submit(event) {
+    event.preventDefault();
+
+    if (edit.id) {
+      const updateTodo = {
+        id: edit.id,
+        activity
+      };
+      const indexTodo = todos.findIndex(value => value.id == edit.id);
+      const updatedTodo = [...todos];
+      updatedTodo[indexTodo] = updateTodo;
+      setTodos(updatedTodo);
+      setActivity("");
+      setEdit({});
+      return;
+    }
+
+    setTodos([...todos, {
+      id: Date.now(),
+      activity
+    }]);
+    setActivity("");
+  }
+
+  function removeTodo(todoid) {
+    const filteredTodos = todos.filter(todo => todo.id !== todoid);
+    setTodos(filteredTodos);
+  }
+
+  function editTodo(item) {
+    setActivity(item.activity);
+    setEdit(item);
+  }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+    className: "my-16 flex flex-wrap justify-evenly w-1/2 h-auto"
+  }, /*#__PURE__*/React.createElement("h1", {
+    className: "text-2xl font-semibold text-slate-800"
+  }, "Todo list"), /*#__PURE__*/React.createElement("form", {
+    action: "",
+    className: "w-fullh-auto"
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "",
+    value: activity,
+    onChange: function (event) {
+      setActivity(event.target.value);
+    },
+    placeholder: "masukan kegiatan anda",
+    className: "outline-none focus:border-b-2 focus:border-sky-300"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit",
+    className: "py-2 px-3 shadow-slate-400 shadow-lg rounded-lg cursor-pointer",
+    onClick: submit
+  }, edit.id ? "simpan perubahan" : "tambah")), /*#__PURE__*/React.createElement("ul", {
+    className: "w-full px-4v list-disc"
+  }, todos.map(item => {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id,
+      className: "p-2"
+    }, item.activity, /*#__PURE__*/React.createElement("button", {
+      className: "py-1 px-2 shadow-slate-300 shadow-md rounded-lg cursor-pointer ml-3",
+      onClick: editTodo.bind(this, item)
+    }, "edit"), /*#__PURE__*/React.createElement("button", {
+      className: "py-1 px-2 shadow-slate-300 shadow-md rounded-lg cursor-pointer ml-3",
+      onClick: removeTodo.bind(this, item.id)
+    }, "hapus"));
+  }))));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Todo, null), todoDiv);
--- src/app.js
+++ src/app.js
@@ -139,16 +139,18 @@ const formDiv = document.getElementById("form")
 
 function Form(){
     const [nama, setNama] = React.useState("")
+    const h1 = React.useRef(null)
     function submit(event){
         event.preventDefault()
+        h1.current.textContent=`terima kasih ${nama}`
         console.log("nama: "+nama)
     }
 
     return<>
-        <div className="flex justify-center bg-slate-200 w-72 rounded-lg mx-auto h-auto py-5 mb-32">
+        <div className="flex justify-center bg-slate-200 w-72 rounded-lg mx-auto h-auto py-5 mb-10">
             <form action="" className="px-4">
                 <label htmlFor="">nama</label>
-                <input type="text" name="nama" id="" className="outline-none border border-sky-300 ring-1 ring-sky-300 rounded-lg px-4 py-1 focus:border-sky-300 focus:border focus:ring-1 focus:ring-sky-300" onChange={function(event){
+                <input type="text" name="nama" value={nama} className="outline-none border border-sky-300 ring-1 ring-sky-300 rounded-lg px-4 py-1 focus:border-sky-300 focus:border focus:ring-1 focus:ring-sky-300" onChange={function(event){
                     setNama(event.target.value)
                 }}/>
                 <div className="py-5 flex justify-center">
@@ -156,6 +158,103 @@ function Form(){
                 </div>
             </form>
         </div>
+        <h1 ref={h1} className="text-center text-slate-800 text-xl font-semibold mb-10"></h1>
     </>
 }
 ReactDOM.render(<Form />, formDiv)
+//fetch API
+const apiDiv = document.getElementById("fetch")
+function Fetch(){
+    let [title, setTitle] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    React.useEffect(()=>{
+        const getData = async ()=>{
+            const getNews = await fetch("https://newsapi.org/v2/everything?q=keyword&apiKey=f2885538eab2402788c28125bc6de076")
+            const news = await getNews.json()
+            const articles = news.articles
+            setTitle(articles)
+            setLoading(false)
+        }
+        getData()
+  
+    },[]) 
+        return <>
+        <h1 className="text-center text-3xl font-bold text-slate-800 mb-5 uppercase">judul berita</h1>
+        <div className="overflow-scroll h-56 w-1/2 mx-auto">
+            <ul className="list-decimal">
+                {loading && <h1 className="text-center text-1xl font-bold text-slate-800 mb-5 uppercase">masih loading bang</h1>}
+                {!loading && title.map((value)=>{
+                    return <li>{value.title}</li>
+                })}
+                
+            </ul>
+        </div>
+        </>
+    
+}
+ReactDOM.render(<Fetch />, apiDiv)
+//to do list 
+const todoDiv = document.getElementById("todolist")
+
+function Todo(){
+    const [activity, setActivity] = React.useState("")
+    const [todos, setTodos] = React.useState([])
+    const [edit ,setEdit] = React.useState({})
+    React.useEffect(function(){
+        console.log(todos)
+    },[todos])
+    function submit(event){
+        event.preventDefault()
+
+        if(edit.id){
+            const updateTodo = {
+                id: edit.id,
+                activity
+            }
+            const indexTodo = todos.findIndex((value)=>value.id == edit.id)
+            const updatedTodo = [...todos]
+            updatedTodo[indexTodo] = updateTodo
+            setTodos(updatedTodo)
+            setActivity("")
+            setEdit({})
+            return
+        }
+        setTodos([...todos,{
+            id : Date.now(),
+            activity
+        }])
+        setActivity("")
+    }
+    function removeTodo(todoid){
+        const filteredTodos = todos.filter((todo)=>todo.id !== todoid)
+        setTodos(filteredTodos)
+    }
+    function editTodo(item){
+        setActivity(item.activity)
+        setEdit(item)
+    }
+    return <>
+        <div className="my-16 flex flex-wrap justify-evenly w-1/2 h-auto">
+            <h1 className="text-2xl font-semibold text-slate-800">Todo list</h1>
+            <form action="" className="w-fullh-auto">
+                <input type="text" name="" value={activity} onChange={function(event){
+                    setActivity(event.target.value)
+                }} placeholder="masukan kegiatan anda" className="outline-none focus:border-b-2 focus:border-sky-300"/>
+
+                <button type="submit" className="py-2 px-3 shadow-slate-400 shadow-lg rounded-lg cursor-pointer" onClick={submit}>{edit.id ? "simpan perubahan":"tambah"}</button>
+            </form>
+            <ul className="w-full px-4v list-disc">
+                {todos.map((item)=>{
+                  return (
+                  <li key={item.id} className="p-2">{item.activity}
+                  <button className="py-1 px-2 shadow-slate-300 shadow-md rounded-lg cursor-pointer ml-3" onClick={editTodo.bind(this, item)}>edit</button>
+                  <button className="py-1 px-2 shadow-slate-300 shadow-md rounded-lg cursor-pointer ml-3" onClick={removeTodo.bind(this,item.id)}>hapus</button>
+                  </li>
+                  )
+                })}
+            </ul>
+        </div>
+    </>
+}
+
+ReactDOM.render(<Todo />, todoDiv)
